9f47b86404b22f5e991f49d496913dcf67812b57
anita-sharma <2016uce1171@mnit.ac.in>
2020-07-19 12:17:22 +0530

    Updates

---
+++ precache-manifest.d4365df568ed37a7576fc4f384efe685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b701238e4e52a8a0c173699c77e416",
+    "url": "/React-dice/index.html"
+  },
+  {
+    "revision": "58695ea17fbb1675c6cf",
+    "url": "/React-dice/static/css/main.2893133d.chunk.css"
+  },
+  {
+    "revision": "1ecf843e0bb81b296534",
+    "url": "/React-dice/static/js/2.763b2280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-dice/static/js/2.763b2280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58695ea17fbb1675c6cf",
+    "url": "/React-dice/static/js/main.d3f60d9e.chunk.js"
+  },
+  {
+    "revision": "b7f13222ccfd8f2c8d0b",
+    "url": "/React-dice/static/js/runtime-main.168c5edb.js"
+  }
+]);
